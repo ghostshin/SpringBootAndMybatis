a9f23d0efe4afb582eb5377fb242fbb0767f1750
shenxu <xlelouchshin@163.com>
2020-06-18 22:36:51 +0800

    注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,13 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//mapper扫描包
+/**
+ * 
+ * 启动类
+ * @author shenxu
+ *
+ */
 @MapperScan("com.example.demo.mapper")
 @SpringBootApplication
 public class DemoApplication {
